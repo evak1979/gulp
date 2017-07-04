2c3c938f79932b1660d1fcf29aef4eb76feea626
Evangelos Aktoudianakis <Evangelos.Aktoudianakis@zopa.com>
2017-07-04 16:17:29 +0100

    Up to browsersync

--- gulp.config.js
+++ gulp.config.js
@@ -1,15 +1,43 @@
 module.exports = function(){
     var client = './src/client/';
-
-    var config = {
-        temp: './.tmp/',
-
+    var clientApp = client + 'app/';
+    var temp = './.tmp/';
+    var server = './src/server/';
+    var config = {        
         allJs:[
         './src/**/*.js',
         './*.js'
         ],
+        client: client,
+        index: client + 'index.html',
+        js: [
+            clientApp + '**/*.module.js',
+            clientApp + '**/*.js',
+            '!' + clientApp + '**/*.spec.js'
+        ],
+        less: [client + 'styles/styles.less'],
+        server: server,
+        temp: temp,
 
-        less: [client + 'styles/styles.less']
+        bower: {
+            json: require('./bower.json'),
+            directory: './bower_components/',
+            ignorePath: '../..'
+        },
+        css: temp + 'styles.css',
+        defaultPort: 7203,
+        nodeServer: './src/server/app.js',
+        browserReloadDelay: 1000
+    };
+
+    config.getWiredepDefaultOptions = function(){
+        var options = {
+            bowerJson: config.bower.json,
+            directory: config.bower.directory,
+            ignorePath: config.bower.ignorePath
+        }
+
+        return options;
     };
 
     return config;
--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var args = require('yargs').argv;
 var del = require('del');
 var gulp = require('gulp');
 var config = require('./gulp.config')();
+var port = process.env.port || config.defaultPort;
+var browserSync = require('browser-sync');
 
 
 var $ = require('gulp-load-plugins')({lazy:true});
@@ -34,6 +36,102 @@ gulp.task('clean-styles', function(done){
 gulp.task('less-watcher', function(){
     gulp.watch([config.less], ['styles']);
 });
+
+gulp.task('inject', ['wiredep', 'styles'] , function(){
+    log('wire up the app css into the html, and call wiredep');    
+
+    return gulp.src(config.index)    
+    .pipe($.inject(gulp.src(config.css)))
+    .pipe(gulp.dest(config.client));
+});
+
+gulp.task('wiredep', function(){
+    log('wire up the bower css and our app js into the html');
+    var options = config.getWiredepDefaultOptions();
+    var wiredep = require('wiredep').stream;
+
+    return gulp.src(config.index)
+    .pipe(wiredep(options))
+    .pipe($.inject(gulp.src(config.js)))
+    .pipe(gulp.dest(config.client));
+});
+
+gulp.task('serve-dev', ['inject'], function(){
+    var isDev = true;
+
+    var nodeOptions = {
+        script: config.nodeServer,
+        delayTime: 1,
+        env: {
+            'PORT': port,
+            'NODE_ENV': isDev ? 'dev' : 'build'
+        },
+        watch: [config.server] // TODO define the files to restart on
+    }
+
+    return $.nodemon(nodeOptions)
+    .on('restart', function(ev){
+        log('*** nodemon restarted');
+        log('logfiles changed on restart:\n' + ev);
+        setTimeout(function(){
+            browserSync.notify('reloading now...');
+            browserSync.reload({stream:false});
+        }, config.browserReloadDelay);
+    })
+    .on('start', ['vet'], function(){
+        log('*** nodemon started');
+        startBrowserSync();
+    })
+    .on('crash', function(){
+        log('*** nodemon died');
+    })
+    .on('exit', function(){
+        log('*** nodemon ended');
+    });    
+});
+
+////////////////////////////
+
+function changeEvent(event){
+    var srcPattern = new RegExp('/,*(?=/' + config.source + ')/');
+    log('File' + event.path.replace(srcPattern, '') + ' ' + event.type);
+}
+
+function startBrowserSync(){
+    if(browserSync.active){
+        return;
+    }
+
+    gulp.watch([config.less], ['styles'])
+    .on('change', function(event){
+        changeEvent(event);
+    });
+
+    var options = {
+        proxy: 'localhost:' + port,
+        files:[
+        config.client +  '**/*.*',
+        '!' + config.less,        
+        config.temp + '**/*.css'
+        ],
+        port: 3000,
+        ghostMode:{
+            clicks:true,
+            location:false,
+            forms:true,
+            scroll:true
+        },
+        injectChanges:true,
+        logFileChanges:true,
+        logLevel: 'debug',
+        logPrefix: 'gulp-patterns',
+        notify: true
+    };
+
+    log('Starting browser-sync on port ' + port);
+
+    browserSync(options);
+}
 
 function clean(path, done) {
     log('Cleaning:' + $.util.colors.blue(path));
